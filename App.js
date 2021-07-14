2ea98734579f8854d182c14a84604e3777895298
ninii007 <86707182+ninii007@users.noreply.github.com>
2021-07-14 15:54:27 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,112 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react';
+import { TextInput, View, Picker, Button } from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Calculator = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [selectedValue, setSelectedValue] = useState("plus");
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const calculate = () => {
+    if(selectedValue == 'plus') {
+      alert(parseFloat(a)+parseFloat(b))
+    } else if(selectedValue == 'minus') {
+      alert(parseFloat(a)-parseFloat(b))
+    } else if(selectedValue == 'multiple') {
+      alert(parseFloat(a)*parseFloat(b))
+    } else if(selectedValue == 'divide') {
+      alert(parseFloat(a)/parseFloat(b))
+    }
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40, borderColor:"#000000", borderWidth:1}}
+        placeholder="Input Number"
+        onChangeText={text => setA(text)}
+      />
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="+" value="plus" />
+        <Picker.Item label="-" value="minus" />
+        <Picker.Item label="x" value="multiple" />
+        <Picker.Item label="/" value="divide" />
+      </Picker>
+      <TextInput
+        style={{height: 40, borderColor:"#000000", borderWidth:1}}
+        placeholder="Input Number"
+        onChangeText={text => setB(text)}
+      />
+      <Button title="Click me!!!" onPress={ calculate } />
+      
     </View>
   );
-};
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default Calculator;
